d63318212f80ba5fe486a7f9eee16ba4971cc55b
Daniel Tobon <shiriux1@gmail.com>
2018-07-09 07:07:35 -0500

    history finished

--- src/views/record.vue
+++ src/views/record.vue
@@ -23,9 +23,9 @@
       <section class="admin-resumen__tabla-resumen">
 
         <p class="admin-resumen__fecha-pedido">
-          <strong>Pedido Del:</strong>
+          <strong>Pedido Del: {{start}}</strong>
           <span class="admin-resumen__fecha-desde"/>
-          <strong>Al:</strong>
+          <strong>Al: {{end}}</strong>
           <span class="admin-resumen__fecha-hasta"/>
         </p>
 
@@ -36,17 +36,29 @@
               Sort
             </div>
             <!-- agregar la clase 'activo' para indicar que se esta ordenando por ese item-->
-            <div class="admin-resumen__ordernar-item activo">
+            <div 
+            @click="setActive('date')"
+            :class="{activo: evaluateActive('date')}"
+            class="admin-resumen__ordernar-item">
               Fecha
               <i class="ion-ios-arrow-down"/>
             </div>
-            <div class="admin-resumen__ordernar-item">
+            <div
+              :class="{activo: evaluateActive('id')}"
+              @click="setActive('id')"
+             class="admin-resumen__ordernar-item">
               ID
             </div>
-            <div class="admin-resumen__ordernar-item">
+            <div 
+            :class="{activo: evaluateActive('state')}"
+              @click="setActive('state')"
+            class="admin-resumen__ordernar-item">
               Estado
             </div>
-            <div class="admin-resumen__ordernar-item">
+            <div 
+            :class="{activo: evaluateActive('price')}"
+              @click="setActive('price')"
+            class="admin-resumen__ordernar-item">
               Monto
             </div>
           </div>
@@ -56,7 +68,7 @@
             <div
               v-for="order in orders"
               :key="order.id"
-              @click="toggleModalDetails"
+              @click="toggleModalDetails(order)"
               class="admin-resumen__lista-item">
               <!-- datos -->
               <div class="admin-resumen__lista-datos-id">
@@ -64,7 +76,7 @@
                   ID <strong>{{order.id}}</strong>
                 </div>
                 <div class="admin-resumen__lista-fecha">
-                  {{setTime(order.created_at)}}
+                  {{setDate(order.created_at)}}
                 </div>
                 <div class="admin-resumen__lista-hora">
                   Para la {{ setTime(order.trackTime) }}
@@ -95,8 +107,12 @@
 
     <!-- modal -->
     <modal-details
+      @close-details="showModalDetails = $event"
       @event-toggle-modal="toggleModalDetails"
-      :toggle-modal="showModalDetails"/>
+      :toggle-modal="showModalDetails"
+      :order-summary="orderSummary"
+      :set-time="setTime"
+      />
   </main>
 </template>
 
@@ -112,6 +128,10 @@ export default {
   },
   data () {
     return {
+      orderSummary: {},
+      activeFilter: '',
+      start: '',
+      end: '',
       orders: [],
       attrs: [
         {
@@ -147,13 +167,95 @@ export default {
         }
       },
       myRange: {
-        start: new Date(2018, 7, 2),
-        end: new Date(2018, 7, 10)
+        start: new Date(2018, 6, 2),
+        end: new Date(2018, 6, 10)
       },
       showModalDetails: false
     }
   },
+  watch: {
+    myRange (value) {
+      console.log(value)
+        const startDate = new Date(value.start)
+      const endDate = new Date(value.end)
+    const dataStart = `${startDate.getFullYear()}-${startDate.getMonth() + 1}-${startDate.getDate()}`
+    const dataEnd = `${endDate.getFullYear()}-${endDate.getMonth() + 1}-${endDate.getDate()}`
+    this.start = dataStart
+    this.end = dataEnd
+      if (dataStart === dataEnd){
+        console.log("hola")
+        const date = new Date(value.start)
+        const data = `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`
+     configService(`central_admin/universities/2/orders?date=${data}`)
+      .then(response => {
+        this.orders = response.data
+        this.setOrderCount()
+        this.setTrackTime()
+        console.log(this.orders)
+      })
+      .catch(error => {
+        console.log(error)
+      })
+      }else{
+       
+     configService(`central_admin/universities/2/orders?start_date=${dataStart}&end_date=${dataEnd}`)
+      .then(response => {
+        this.orders = response.data
+        console.log(this.orders)
+         this.setOrderCount()
+        this.setTrackTime()
+      })
+      .catch(error => {
+        console.log(error)
+      })
+      }
+    }
+  },
   methods: {
+    sortBy (type) {
+      switch (type) {
+        case 'id':
+            this.orders = this.orders.sort((a, b) => {
+              return b.id - a.id
+            })
+          break
+        case 'price':
+            this.orders = this.orders.sort((a, b) => {
+              return b.total - a.total
+            })
+          break
+        case 'status':
+            this.orders = this.orders.sort((a, b) => {
+              if (a.status > b.status) {
+                return 1
+              }
+            })
+          break
+           case 'date':
+            this.orders = this.orders.sort((a, b) => {
+              return new Date(a.created_at) - new Date(b.created_at)
+            })
+          break
+        default:
+          break
+      }
+    },
+    evaluateActive (filter) {
+      if (this.activeFilter === filter){
+        return true
+      }
+    },
+    setActive (filter) {
+      this.activeFilter = filter
+      this.sortBy(filter)
+    },
+    setDate(date) {
+      const data = new Date(date)
+      return `${data.getFullYear()}/${data.getMonth() + 1}/${data.getDate()}`
+    },
+    setRange(){
+      console.log(this.myRange)
+    },
     setTime (date) {
       const dateFormatted = new Date(date)
       let minutes = dateFormatted.getMinutes()
@@ -164,8 +266,9 @@ export default {
       const createdTime = hour > 12 ? `${(hour - 12)}:${minutes}pm` : `${(hour)}:${minutes}am`
       return createdTime
     },
-    toggleModalDetails () {
-      this.showModalDetails = !this.showModalDetails
+    toggleModalDetails (order) {
+      this.showModalDetails = true
+      this.orderSummary = order
     },
     setOrderCount () {
      this.orders = this.orders.map((element, index) => {

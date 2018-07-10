e35c560bf7615e73b4ade0b8fb3450d6b18168bb
Daniel Tobon <shiriux1@gmailcom>
2018-07-10 16:17:54 -0500

    fixed identation

--- src/components/today/modal-details.vue
+++ src/components/today/modal-details.vue
@@ -10,13 +10,13 @@
     <div class="modal-admin__header">
       <h3>{{ orderSummary.id }}</h3>
       <h2>{{ orderSummary.commerce.commercial_name }}</h2>
-      <p 
-       v-if="orderSummary.status !== 'order_completed'"
-      class="modal-admin__hora">
-        Para {{ setTime(orderSummary.trackTime) }}
+      <p
+        v-if="orderSummary.status !== 'order_completed'"
+        class="modal-admin__hora">
+        {{ setTime(orderSummary.trackTime,orderSummary.deliveryDiference) }}
       </p>
       <!-- colocar la clase "activo" para indicar que fue aceptado el pedido -->
-      <h4 class="modal-admin__estado-pedido activo">{{orderSummary.status}}</h4>
+      <h4 class="modal-admin__estado-pedido activo">{{ orderSummary.status }}</h4>
     </div>
     <!-- lista del pedido -->
     <div class="modal-admin__lista">
@@ -28,7 +28,7 @@
         class="modal-admin__lista-item">
         <!-- numero producto en la listaa -->
         <div class="modal-admin__lista-num">
-          {{product.count}}
+          {{ product.count }}
         </div>
         <!-- informacion del producto -->
         <div class="modal-admin__lista-info">
@@ -41,14 +41,14 @@
             </li>
 
           </ul>
-          
+
           <h5 class="modal-admin__lista-comentario">Comentarios:</h5>
           <p class="modal-admin__lista-mensaje">
             Enviar salsa de tomate y mayonesa, no incluir la cebolla
           </p>
-          
+
           <div class="modal-admin__lista-precio">
-              {{ product.total_price | currency('$', 0) }}
+            {{ product.total_price | currency('$', 0) }}
           </div>
         </div>
 
@@ -60,7 +60,7 @@
       <div class="modal-admin__row">
         <p class="modal-admin__cupon">
           <strong>Cupón</strong>
-          {{setCoupon(orderSummary)}}
+          {{ setCoupon(orderSummary) }}
         </p>
         <div class="modal-admin__domicilio">
           Sin domicilio
@@ -70,7 +70,7 @@
         v-if="orderSummary.is_takeout"
         class="modal-admin__row">
         <strong>Takeout</strong>
-         {{ orderSummary.takeout | currency('$', 0) }}
+        {{ orderSummary.takeout | currency('$', 0) }}
       </p>
       <p
         v-else
@@ -79,7 +79,7 @@
         {{ orderSummary.delivery_price | currency('$', 0) }}
       </p>
       <div class="modal-admin__total">
-         TOTAL COP {{ orderSummary.total | currency('$', 0) }}
+        TOTAL COP {{ orderSummary.total | currency('$', 0) }}
       </div>
     </div>
     <!-- formad de pagos -->
@@ -89,40 +89,40 @@
           Forma de pago
         </div>
         <div class="modal-admin__pago-modo">
-          {{orderSummary.payment_type}} <i class="ion-ios-arrow-forward"/>
+          {{ orderSummary.payment_type }} <i class="ion-ios-arrow-forward"/>
         </div>
       </div>
       <div
-      v-if="!orderSummary.is_takeout"
-       class="modal-admin__pagos-row">
+        v-if="!orderSummary.is_takeout"
+        class="modal-admin__pagos-row">
         <div class="modal-admin__pago">
           Runner
         </div>
         <div class="modal-admin__pago-modo">
-          {{setDelivery(orderSummary.delivery)}} <i class="ion-ios-arrow-forward"/>
+          {{ setDelivery(orderSummary.delivery) }} <i class="ion-ios-arrow-forward"/>
         </div>
       </div>
     </div>
     <!-- estados -->
     <div class="modal-admin__estados">
       <!-- item -->
-      <div 
-      @click="setState(button,orderSummary)"
-      v-for="button in orderSummary.buttons"
-      :key="button"
-      class="modal-admin__estado-item">
-        {{button}}
+      <div
+        @click="setState(button,orderSummary)"
+        v-for="button in orderSummary.buttons"
+        :key="button"
+        class="modal-admin__estado-item">
+        {{ button }}
       </div>
     </div>
     <!-- informacion base -->
     <div class="modal-admin__info">
       <p>
-        <strong>Cliente: </strong>{{setName(orderSummary)}}
-        ({{orderSummary.campus_email}})
+        <strong>Cliente: </strong>{{ setName(orderSummary) }}
+        ({{ orderSummary.campus_email }})
       </p>
       <p>
         <strong>Restaurante: </strong>
-        {{orderSummary.commerce.commercial_name}} {{orderSummary.commerce.telephone}}
+        {{ orderSummary.commerce.commercial_name }} {{ orderSummary.commerce.telephone }}
       </p>
     </div>
   </div>
@@ -153,89 +153,88 @@ export default {
     setTime: {
       type: Function,
       default: () => {
-        return 
+
       }
     }
   },
   methods: {
     setTrackTime () {
-       const createdTime = new Date(this.orderSummary.created_at)
-        const trackDeliveryMinutes = parseInt(this.orderSummary.commerce.avg_delivery_time)
-        const trackPreparationMinutes = parseInt(this.orderSummary.commerce.avg_preparation_time)
-        const trackTime = new Date(createdTime.getFullYear(), createdTime.getMonth(),
+      const createdTime = new Date(this.orderSummary.created_at)
+      const trackDeliveryMinutes = parseInt(this.orderSummary.commerce.avg_delivery_time)
+      const trackPreparationMinutes = parseInt(this.orderSummary.commerce.avg_preparation_time)
+      const trackTime = new Date(createdTime.getFullYear(), createdTime.getMonth(),
         createdTime.getDate(), createdTime.getHours(), createdTime.getMinutes() + trackDeliveryMinutes + trackPreparationMinutes)
-        const newElement = {...this.orderSummary, trackTime: trackTime}
-        this.orderSummary = newElement
+      const newElement = {...this.orderSummary, trackTime: trackTime}
+      this.orderSummary = newElement
     },
-    calculateStateButtons(order){
+    calculateStateButtons (order) {
       let buttons = []
-       switch (order.status) {
+      switch (order.status) {
         case 'waiting_for_external_payment':
-          buttons =  ['send_to_restaurant','invalid_payment']
-          break;
+          buttons = ['send_to_restaurant', 'invalid_payment']
+          break
         case 'waiting_restaurant_confirmation':
-        buttons = ['accept_order','reject_order'] 
-         break;
+          buttons = ['accept_order', 'reject_order']
+          break
         case 'preparing_order':
-        buttons = ['dispatch_order']
-        break;
+          buttons = ['dispatch_order']
+          break
         case 'waiting_pickup_client':
-        buttons = ['complete_order','reject_order'] 
-         break;
+          buttons = ['complete_order', 'reject_order']
+          break
         case 'waiting_pickup_deliveryman':
-        buttons = ['pickup_order']
-        break;
+          buttons = ['pickup_order']
+          break
         case 'delivering_order':
-        buttons = ['complete_order','problem_with_delivery','problem_with_hand_off']
-        break;
+          buttons = ['complete_order', 'problem_with_delivery', 'problem_with_hand_off']
+          break
         case 'troubleshooting_deliveryman':
-        buttons = ['pickup_order','cancel_order']
-        break;
+          buttons = ['pickup_order', 'cancel_order']
+          break
         case 'troubleshooting_hand_off':
-        buttons = ['complete_order','cancel_order']
-        break;
+          buttons = ['complete_order', 'cancel_order']
+          break
         case 'troubleshooting_restaurant':
-        buttons = ['accept_order','cancel_order']
-        break;
+          buttons = ['accept_order', 'cancel_order']
+          break
         default:
-          break;
+          break
       }
-      this.orderSummary = {...order,buttons: buttons}
+      this.orderSummary = {...order, buttons: buttons}
       console.log(this.orderSummary)
     },
-    setState(button,order) {
+    setState (button, order) {
       const data = {
         commerce_id: order.commerce.id,
-        comments: "cualquier comentario"
+        comment: 'cualquier problema'
       }
-     configService.post(`central_admin/orders/${order.id}/${button}`,data)
-      .then(response => {
-        const newOrder = response.data
-        this.calculateStateButtons(newOrder)
-        this.setTrackTime()
-         this.$emit('order-modal', this.orderSummary)
-      })
-      .catch(error => {
-        console.log(error)
-      })
+      configService.post(`central_admin/orders/${order.id}/${button}`, data)
+        .then(response => {
+          const newOrder = response.data
+          this.calculateStateButtons(newOrder)
+          this.setTrackTime()
+          this.$emit('order-modal', this.orderSummary)
+        })
+        .catch(error => {
+          console.log(error)
+        })
     },
-    setName(order){
+    setName (order) {
       return `${order.first_name} ${order.last_name}`
     },
-    setCoupon(order) {
+    setCoupon (order) {
       if (order.coupon_name) {
         return order.coupon_name
-      }else{
+      } else {
         return 'Sin cupon'
       }
     },
-    setDelivery (delivery){
-      if (delivery.delivery_man){
-         return `${delivery.delivery_man.first_name} ${delivery.delivery_man.last_name}`
-      }else {
+    setDelivery (delivery) {
+      if (delivery.delivery_man) {
+        return `${delivery.delivery_man.first_name} ${delivery.delivery_man.last_name}`
+      } else {
         return 'No Asignado'
       }
-     
     },
     addonNames (addon) {
       let name = ''
